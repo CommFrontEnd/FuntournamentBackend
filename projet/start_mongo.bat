cda2a76e5a1488e3f2194d2971f29bed7c23119f
Communauté FRONT-END <comm-frontend@sii.fr>
2017-03-10 13:05:29 +0100

    suppression balise merge

--- projet/start_mongo.bat
+++ projet/start_mongo.bat
@@ -1,8 +1,3 @@
 C:
-<<<<<<< HEAD
 cd C:\Program Files\MongoDB\Server\3.4\bin
 mongod --dbpath "D:\Communauté Front\FunTurnament\FuntournamentBackend\projet\database\v1"
-=======
-cd C:\Program Files\MongoDB\Server\3.2\bin
-mongod --dbpath "D:\Communaute Front\FunTournament\FuntournamentBackend\projet\database\v1"
->>>>>>> 8d40eba701c1da8ce860cf889febe639f1018f40
