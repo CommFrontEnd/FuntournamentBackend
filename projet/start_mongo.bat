ab1058b405c2bac7c2ddcbbe0e9835386ad21507
Communauté FRONT-END <comm-frontend@sii.fr>
2017-03-10 13:08:19 +0100

    suppression accent

--- projet/start_mongo.bat
+++ projet/start_mongo.bat
@@ -1,3 +1,3 @@
 C:
 cd C:\Program Files\MongoDB\Server\3.4\bin
-mongod --dbpath "D:\Communauté Front\FunTurnament\FuntournamentBackend\projet\database\v1"
+mongod --dbpath "D:\Communaute Front\FunTurnament\FuntournamentBackend\projet\database\v1"
